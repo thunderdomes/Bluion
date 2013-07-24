dd848b61f6b0992e7b3e281538ffef7566e707b9
Arie <arie@wego.com>
2013-07-24 21:30:37 +0700

    init simamaung

---
+++ Simamaung/Simamaung/netraAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  netraAppDelegate.h
+//  Simamaung
+//
+//  Created by Arie on 7/24/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface netraAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
