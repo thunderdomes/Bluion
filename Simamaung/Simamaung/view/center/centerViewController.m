0c7831f0e24fd4c82cce007060d9af18b38b05d1
Arie <arie@wego.com>
2013-08-01 11:30:47 +0700

    add center

---
+++ Simamaung/Simamaung/view/center/centerViewController.m
@@ -0,0 +1,13 @@
+//
+//  centerViewController.m
+//  Simamaung
+//
+//  Created by Arie on 8/1/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import "centerViewController.h"
+
+@implementation centerViewController
+
+@end
